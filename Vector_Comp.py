Vector_Comp.py - Bug fixing - apply scaling factor based on sin & cos of alpha

Vector_Comp.py
@@ -6,25 +6,26 @@ from Transformation_Matrix import comp_matrix
 
 translation_offset = np.empty((2), dtype=float)											  # Needs to be upped to 3 for 3d
 uniform_scale_factor = 0
+#scale_factor = np.ones(3)
 
 def find_scale (pts):																	  # TODO: check for accuracy, close, but off in testing
 	global translation_offset
 	global uniform_scale_factor
+	#global scale_factor
 	dist_0 = 0
 	dist_1 = 0
 	if (pts.shape == (2,2,3)):
 		dist_0 = np.sqrt((pts[0,1,0]-pts[0,0,0])**2 + (pts[0,1,1]-pts[0,0,1])**2 + (pts[0,1,2]-pts[0,0,2])**2)	# Length of first simplex (3d)	for first vector: sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
 		dist_1 = np.sqrt((pts[1,1,0]-pts[1,0,0])**2 + (pts[1,1,1]-pts[1,0,1])**2 + (pts[1,1,2]-pts[1,0,2])**2)	# Length of second simplex (3d)
 		uniform_scale_factor = (dist_0/dist_1)
-		translation_offset += np.append((uniform_scale_factor)*pts[0,0],0)
+		scale_factor = np.array([pts[0,0,0]/pts[1,0,0], pts[0,0,1]/pts[1,0,1], pts[0,0,2]/pts[1,0,2]])
+		#translation_offset += np.append((uniform_scale_factor*pts[1,0]) - pts[1,0],0)
 		return uniform_scale_factor*np.ones(3)
 	else: 
 		dist_0 = np.sqrt((pts[0,1,0]-pts[0,0,0])**2 + (pts[0,1,1]-pts[0,0,1])**2)		  # Length of first simplex (2d)	for first vector: sqrt((x2-x1)**2+(y2-y1)**2)
 		dist_1 = np.sqrt((pts[1,1,0]-pts[1,0,0])**2 + (pts[1,1,1]-pts[1,0,1])**2)		  # Length of second simplex (2d)
 		uniform_scale_factor = (dist_0/dist_1)
-		translation_offset += ((uniform_scale_factor)*pts[0,0]) - pts[0,0]
-		#print('target_pt', pts[0,0])
-		print('uniform_scale_factor', dist_1/dist_0)
+		print('uniform_scale_factor', 1/uniform_scale_factor)
 		print('scale_translation_offset_contribution',translation_offset)
 		return uniform_scale_factor*np.ones(2)
 	
@@ -38,22 +39,17 @@ def find_rot (pts):
 
 	dx = pts[1,0,0] - pts[0,0,0]
 	dy = pts[1,0,1] - pts[0,0,1]
-	
-	#print ('dx dy', dx, dy)
-	#if (pts.shape == (2,2,3)):															  # get raw translation distances
-	#	temp_translation_off = -(np.append(pts[1,0] - pts[0,0],0))
-	#else:
-	#	temp_translation_off = -(pts[1,0] - pts[0,0])
-
 
 	alpha = np.arctan2(v1[1],v1[0]) - np.arctan2(v0[1],v0[0])							  # alpha angle about x-axis of standard basis (rad)
-	r = np.sqrt(pts[0,0,0]**2 + pts[0,0,1]**2)											  # TODO: This needs to be calculate off of the point to be rotated BEFORE translation, not the alignment pt from S1
+	r = np.sqrt(pts[1,0,0]**2 + pts[1,0,1]**2)											  # TODO: This needs to be calculate off of the point to be rotated BEFORE translation, not the alignment pt from S1
 	alpha_prime = np.arctan2(v1[1],v1[0])												  # angle to alpha from x+
-	s2_align_pt = np.array([((r) * np.cos(-alpha_prime)),
-	 ((r) * np.sin(-alpha_prime))])														  # point as calculated from standard basis
-	s2_align_pt *= uniform_scale_factor
+	s2_align_pt = np.array([((r) * np.cos(-alpha)),
+	 ((r) * np.sin(-alpha))])														 	  # point as calculated from standard basis
+	s2_align_pt /= uniform_scale_factor
+	#s2_align_pt = s2_align_pt +  np.multiply(uniform_scale_factor-1 * np.array([np.cos(alpha),np.sin(alpha)]), s2_align_pt)		# !!!!!! TODO: apply scale based axis contribution?
 	print ('target_alignment_pt', pts[0,0])
-	#print ('alpha r', alpha, r)
+	print ('alpha r', alpha, r)
+	print ('s2_align_pt_r', np.sqrt(s2_align_pt[0]**2 + s2_align_pt[1]**2))
 	#print ('alpha_prime', alpha_prime)
 	print ('s2_align_pt', s2_align_pt)
 	#print ('usf',usf)
@@ -107,9 +103,11 @@ def find_trans (pts):																	  # todo: add in the translation_offset fo
 	if (pts.shape == (2,2,3)):
 		#return -(np.append(pts[1,0] - pts[0,0],0)) #+ translation_offset 
 	#return -(pts[1,0] - pts[0,0]) #+ translation_offset 
-		return - translation_offset
-	return - np.array([pts[1,0,0] - pts[0,0,0], pts[1,0,1] - pts[0,0,1]])# + translation_offset
-	
+		#return - translation_offset
+	#return - translation_offset # * uniform_scale_factor #np.array([pts[1,0,0] - pts[0,0,0], pts[1,0,1] - pts[0,0,1]])# + translation_offset
+		return np.array([0,0,0])
+	return np.array([0,0,0])
+
 def find_transf_matrix (pts):															  # Do a check that we are getting 2 or 3 dimentional crds
 	print('pts', pts)
 	global translation_offset															  # Create a touple with correct dimentions bsed on input pts